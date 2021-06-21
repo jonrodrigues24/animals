f08078ed8509268f67b2c5148b7e631928e15503
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-06-21 15:16:52 -0600

    Imageview (emulator not tested)

--- app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
@@ -9,23 +9,24 @@ import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.R;
 import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.service.WebServiceProxy;
 import java.io.IOException;
 import java.util.List;
-import java.util.Random;
 import retrofit2.Response;
 
 
 //the controller
 public class MainActivity extends AppCompatActivity {
 
-  private WebView contentView;
+  private ImageView image;
   private Spinner animalSelector;
   private ArrayAdapter<Animal> adapter;
 
@@ -35,14 +36,14 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    contentView = findViewById(R.id.content_view);
+    image = findViewById(R.id.image);
     animalSelector = findViewById(R.id.animal_selector);
     animalSelector.setOnItemSelectedListener(new OnItemSelectedListener() {
       @Override
       public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
 
         Animal animal = (Animal) adapterView.getItemAtPosition(position);
-        contentView.loadUrl(animal.getImageUrl());
+        Picasso.get().load(animal.getImageUrl()).into(image);
 
       }
 
@@ -51,27 +52,12 @@ public class MainActivity extends AppCompatActivity {
 
       }
     });
-    setupWebView();
-
-
-  }
-//web view settings to fit device
-  private void setupWebView() {
-
-    contentView.setWebViewClient(new WebViewClient() {
-      @Override
-      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-        return false;
-      }
-    });
-    WebSettings settings = contentView.getSettings();
-    settings.setSupportZoom(true);
-    settings.setBuiltInZoomControls(true);
-    settings.setDisplayZoomControls(false);
-    settings.setUseWideViewPort(true);
-    settings.setLoadWithOverviewMode(true);
     new Retriever().start();
+
+
   }
+//web view settings to fit device deleted
+
 // runs the app
   private class Retriever extends Thread {
 
